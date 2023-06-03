6f3ab01e8ea976487ceed36ac0faee9d10f6b867
Daniil Savinov <savinovdd@gmail.com>
2023-06-03 11:59:51 +0300

    Шаблон

--- Main.py
+++ Main.py
@@ -1,11 +1,22 @@
-цепь на мне
-сыпь лаве
-сотка тыщ в бэк лв
-сотни сук хотят ко мне
-сотни сук катят ко мне
-эй как дела
-как дела
-это новый кадилак
-делать деньги
-делать деньги
-делать деньги блять вот так
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard
+import requests
+
+session = requests.Session()
+
+LOGIN = '+79179321974'
+PASSWORD = 'sokolbond007'
+
+vk_session = vk_api.VkApi(LOGIN, PASSWORD)
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+
+# try:
+#     vk_session.auth(token_only=True)
+# except vk_api.AuthError as error_msg:
+#     print(error_msg)
+
+keyboard = VkKeyboard(one_time=False, inline=True)
+keyboard.add_callback_button(label="Open URL",
+                             payload={"type": "open_link", "link": "https://vk.com/dev/bots_docs_5"})
